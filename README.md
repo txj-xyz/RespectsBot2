2c95c3a8b032129afc8cb6734783454b197eddaa
0x0b6e48 <jfinelli3@gmail.com>
2020-04-23 11:29:24 -0600

    Inital push of V2
    probably not going to make any more changes.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # RespectsBot2
  Simple remake of the original bot on Discord.JS. Responds only to F
+
+
+## Installation
+
+ - Clone this repo and use `npm i`
+ - Start the bot from `app.js`
+
+ 
+## Requirements
+
+```
+discord.js
+node.js
+```
---
+++ app.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//Store destructive count.
+var respectCount = 0;
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//Wait for F message.
+client.on('message', msg => {
+	
+  if (msg.content === 'f') {
+	  
+	respectCount++
+    msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${respectCount}`);
+	console.log(`Respect has been paid, Count: ${respectCount}`);
+  
+  }else if(msg.content === 'F'){
+	
+	respectCount++
+	msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${respectCount}`)
+	console.log(`Respect has been paid, Count: ${respectCount}`);
+	
+  }
+  
+});
+
+
+//Simple Token Login.
+client.login('<BOT_TOKEN>');
