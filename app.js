3c755abafd8a9cd24d075ebb42ad0174bee52c39
0x0b6e48 <jfinelli3@gmail.com>
2020-04-23 13:37:55 -0600

    logging change version bump

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ client.on('message', msg => {
 	  
 	respectCount++
     msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${respectCount}`);
-	console.log(`Respect has been paid, Count: ${respectCount}`);
+	console.log(`Respect has been paid, Count: ${respectCount} User ${msg.author.username}`);
   
   }else if(msg.content === 'F'){
 	
 	respectCount++
 	msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${respectCount}`)
-	console.log(`Respect has been paid, Count: ${respectCount}`);
+	console.log(`Respect has been paid, Count: ${respectCount} User ${msg.author.username}`);
 	
   }
   
