ae4f5c4086112559c2426697644f33e3cffdcbfb
0x0b6e48 <jfinelli3@gmail.com>
2020-04-24 13:35:16 -0600

    File based storage changes.
    In the coming releases I will include a "serverCount" and "userCount" as well as maybe some channel blacklisting.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
+const fs = require('fs');
+const storageFile = require('./arrStorage.json');
 const client = new Discord.Client();
 
-//Store destructive count.
-var respectCount = 0;
-
+//Re-did the local storage to a file based storage.
+confFile = storageFile;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -14,20 +15,21 @@ client.on('message', msg => {
 	
   if (msg.content === 'f') {
 	  
-	respectCount++
-    msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${respectCount}`);
-	console.log(`Respect has been paid, Count: ${respectCount} User ${msg.author.username}`);
+	confFile["respectCount"]++
+	fs.writeFileSync('./arrStorage.json', JSON.stringify(confFile, null, '\t'))
+    msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${confFile["respectCount"]}`);
+	console.log(`Respect has been paid, Count: ${confFile["respectCount"]} User ${msg.author.username}`);
   
   }else if(msg.content === 'F'){
 	
-	respectCount++
-	msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${respectCount}`)
-	console.log(`Respect has been paid, Count: ${respectCount} User ${msg.author.username}`);
+	confFile["respectCount"]++
+	fs.writeFileSync('./arrStorage.json', JSON.stringify(confFile, null, '\t'))
+	msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${confFile["respectCount"]}`)
+	console.log(`Respect has been paid, Count: ${confFile["respectCount"]} User ${msg.author.username}`);
 	
   }
   
 });
-
 
 //Simple Token Login.
 client.login('<BOT_TOKEN>');
