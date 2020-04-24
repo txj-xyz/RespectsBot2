f6caef16c83c819724a5a7d9dfc8f41920b7269c
0x0b6e48 <jfinelli3@gmail.com>
2020-04-24 16:51:42 -0600

    formatting and easter egg?

--- app.js
+++ app.js
@@ -7,28 +7,31 @@ const client = new Discord.Client();
 localFile = storageFile;
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 //Wait for F message.
 client.on('message', msg => {
 	
-  if (msg.content === 'f') {
+	if (msg.content === 'f') {
 	  
-	confFile["respectCount"]++
-	fs.writeFileSync('./arrStorage.json', JSON.stringify(localFile, null, '\t'))
-    msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${localFile["respectCount"]}`);
-	console.log(`Respect has been paid, Count: ${localFile["respectCount"]} User ${msg.author.username}`);
-  
-  }else if(msg.content === 'F'){
+		confFile["respectCount"]++
+		fs.writeFileSync('./arrStorage.json', JSON.stringify(localFile, null, '\t'))
+		msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${localFile["respectCount"]}`);
+		console.log(`Respect has been paid, Count: ${localFile["respectCount"]} User ${msg.author.username}`);
+
+	}else if(msg.content === 'F'){
+
+		confFile["respectCount"]++
+		fs.writeFileSync('./arrStorage.json', JSON.stringify(localFile, null, '\t'))
+		msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${localFile["respectCount"]}`)
+		console.log(`Respect has been paid, Count: ${localFile["respectCount"]} User ${msg.author.username}`);
 	
-	confFile["respectCount"]++
-	fs.writeFileSync('./arrStorage.json', JSON.stringify(localFile, null, '\t'))
-	msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${localFile["respectCount"]}`)
-	console.log(`Respect has been paid, Count: ${localFile["respectCount"]} User ${msg.author.username}`);
-	
-  }
-  
+	}else if(e.message.content.indexOf("<!@468171246018756609>") >= 0){
+		e.message.channel.sendMessage(":eyes:")
+		return
+	}
+
 });
 
 //Simple Token Login.
