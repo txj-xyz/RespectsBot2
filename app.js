5e689954a8d61e9ac04621ebfebf2c61de8bf801
0x0b6e48 <jfinelli3@gmail.com>
2020-04-27 21:11:19 -0600

    removed @mention and formatting

--- app.js
+++ app.js
@@ -12,32 +12,27 @@ client.on('ready', () => {
 
 //Wait for F message.
 client.on('message', msg => {
-	
-	//try to send the message if error then return it and stop the command.
 	try{
 		if (msg.content === 'f') {
-	  
-		localFile["respectCount"]++
-		fs.writeFileSync('./arrStorage.json', JSON.stringify(localFile, null, '\t'))
-		msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${localFile["respectCount"]}`);
-		console.log(`Respect has been paid, Count: ${localFile["respectCount"]} User ${msg.author.username}`);
+			localFile["respectCount"]++
+			fs.writeFileSync('./arrStorage.json', JSON.stringify(localFile, null, '\t'))
+			msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${localFile["respectCount"]}`);
+			console.log(`Respect has been paid, Count: ${localFile["respectCount"]} User ${msg.author.username}`);
 
-	}else if(msg.content === 'F'){
+		}else if(msg.content === 'F'){
 
-		localFile["respectCount"]++
-		fs.writeFileSync('./arrStorage.json', JSON.stringify(localFile, null, '\t'))
-		msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${localFile["respectCount"]}`)
-		console.log(`Respect has been paid, Count: ${localFile["respectCount"]} User ${msg.author.username}`);
-	
-	}else if(msg.content.indexOf("<!@468171246018756609>") >= 0){
-		msg.reply(":eyes:")
-		return
-	}
+			localFile["respectCount"]++
+			fs.writeFileSync('./arrStorage.json', JSON.stringify(localFile, null, '\t'))
+			msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${localFile["respectCount"]}`)
+			console.log(`Respect has been paid, Count: ${localFile["respectCount"]} User ${msg.author.username}`);
+		
+		}else if(msg.content === "<!@468171246018756609>"){
+			msg.reply(":eyes:")
+		}
 	}catch(e){
 		console.log(`Error found with sending message, dumping.`)
 		console.log(e)
 	}
-
 });
 
 //Simple Token Login.
