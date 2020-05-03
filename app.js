5c586d0dcf75b0774f739b9767c225ec47f4a00b
0x0b6e48 <jfinelli3@gmail.com>
2020-05-02 21:52:46 -0600

    COMPLETE REWRITE, HOLY FUCK INCOMING BOYS

--- app.js
+++ app.js
@@ -1,37 +1,120 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-const storageFile = require('./arrStorage.json');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const si = require('systeminformation')
+const humanizeDuration = require('humanize-duration')
+const cfg = require("./config.json")
+const DBL = require("dblapi.js")
+const mongo = require("mongodb").MongoClient
 
-//Re-did the local storage to a file based storage.
-localFile = storageFile;
+//Spawn Clients
+const client = new Discord.Client()
+const dbl = new DBL(cfg.bot.dblToken, client)
 
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-});
+//connect over to mongo.
+function connectDB() {
+  return new Promise((resolve, reject) => {
+    mongo.connect(`mongodb://${cfg.database.host}:${cfg.database.port}`, async (err, data) => {
+      if(err) return console.log('Error connecting to mongodb')
+      resolve(data.db(cfg.database.db))
+    })
+  })
+}
 
-//Wait for F message.
-client.on('message', msg => {
-	try{
-		if (msg.author.bot == false && msg.content === 'f') {
-			localFile["respectCount"]++
-			fs.writeFileSync('./arrStorage.json', JSON.stringify(localFile, null, '\t'))
-			msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${localFile["respectCount"]}`);
-			console.log(`Respect has been paid, Count: ${localFile["respectCount"]} User ${msg.author.username}`);
+//Global vars for sys info command.
+let cpu
+let mem
+let operating
 
-		}else if(msg.author.bot == false && msg.content === 'F'){
+//on client ready, connect over to the db for mongo and get ready to store data.
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+  
+  //assign mongodb cursor to client.database for use elsewhere
+  client.database = await connectDB()
 
-			localFile["respectCount"]++
-			fs.writeFileSync('./arrStorage.json', JSON.stringify(localFile, null, '\t'))
-			msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\n:bar_chart: ${localFile["respectCount"]}`)
-			console.log(`Respect has been paid, Count: ${localFile["respectCount"]} User ${msg.author.username}`);
-		
-		}
-	}catch(e){
-		console.log(`Error found with sending message, dumping.`)
-		console.log(e)
-	}
-});
+  setInterval(async () => {
+    dbl.postStats(client.guilds.cache.size)
+    .catch(e => console.log(`Error posting stats: ${e}`))
+  }, 1800000)
 
-//Simple Token Login.
-client.login('<BOT_TOKEN>');
+  cpu = await si.cpu()
+  mem = await si.mem()
+  operating = await si.osInfo()
+})
+
+client.on('message', async msg => {
+
+  //pull stats for bot :thinking:
+  if(msg.content.toLowerCase() === 'rb!info'){
+    msg.reply(
+      new Discord.MessageEmbed()
+        .setTitle('Statistics')
+        .setColor('#fcebb3')
+        .setTimestamp()
+        .setFooter(msg.author.tag)
+        .addField('Users', `\`${client.users.cache.size}\``, true)
+        .addField('Guilds', `\`${client.guilds.cache.size}\``, true)
+        .addField('Language', '`NodeJS`', true)
+        .addField('RAM\'s', `\`${Math.floor(mem.used / 1000000000)}GB/${Math.floor(mem.total / 1000000000)}GB\``, true)
+        .addField('CPU', `\`${cpu.cores} Cores\``, true)
+        .addField('Platform', `\`${operating.platform}\``, true)
+        .addField('Shards', `\`${parseInt(client.options.shards) + 1}\``, true)
+        .addField('Ping', `\`${client.ws.ping} ms\``, true)
+        .addField('Uptime', `\`${humanizeDuration(client.uptime)}\``, true)
+        .addField('Developer', '`TXJ#0001`', true)
+    )
+  }
+
+  if(msg.content.toLowerCase() === 'f' && !msg.author.bot){
+
+    //If client.database doesnt exist that means our database isnt connected
+    if(!client.database) return msg.reply('Error: Database not connected!')
+
+    //Check to make sure they exist in the database
+    let guildEntry = await client.database.collection('guilds').findOne({guild_id: msg.guild.id})
+    let fcount = 0
+
+    //If the dont make them a new object and assign fcount to 1
+    if(!guildEntry) {
+      await client.database.collection('guilds').insertOne({
+        guild_id: msg.guild.id,
+        fcount: 1
+      })
+      fcount = 1
+    }
+
+    //If they do modify the object fcount++
+    else {
+      await client.database.collection('guilds').updateOne({
+        guild_id: msg.guild.id
+      }, {
+        $set: { fcount: guildEntry.fcount + 1 }
+      })
+      
+      fcount = guildEntry.fcount + 1
+    }
+   
+    let allEntries = await client.database.collection('guilds').find({}).toArray()
+
+    let total = 80976;
+    allEntries.forEach(e => {
+      total += e.fcount
+    })
+   
+    //do stuff here with mesage
+    console.log(`Guild - ${msg.guild.name} : user - ${msg.author.tag} : fcount - ${fcount}`)
+
+    //New Embed format! PogChamp
+    msg.channel.send(new Discord.MessageEmbed()
+    .setTitle('Respect Found')
+    .setDescription(`<@${msg.author.id}> has paid their respects. :pray: :regional_indicator_f:`)
+    .setColor('#003cff')
+    .setTimestamp()
+    //.setFooter(msg.author.tag)
+    .addField('Server Respects', `\`${fcount}\``, true)
+    .addField('Total Respects', `\`${total}\``, true)
+    //msg.reply(`has paid their Respects. :regional_indicator_f: :pray:\nTotal Respects: \`${total}\`\nRespects Paid on this guild: \`${fcount}\``)
+    )
+  }
+})
+
+client.login(cfg.bot.token)
