58334dcf737428bfe2b0d15df0da580873cb3447
JayEffTree <jfinelli3@gmail.com>
2020-05-18 21:36:38 +0200

    async loading from array instead of hardcode, music soon

--- commands/help.js
+++ commands/help.js
@@ -1,33 +1,31 @@
-const Discord = require('discord.js')
 const cfg = require("../config/config.json")
+const helpList = [];
 
 module.exports = {
 	name: 'help',
 	description: 'Shows all commands for the bot',
-	execute(client, msg) {
-                msg.reply(
-                        new Discord.MessageEmbed()
-                        .setTitle('Respects Bot - Commands')
-                        .setColor('#ffff4f')
-                        .setTimestamp()
-                        .setDescription(
-                        `If you would like to pay your respects:tm: simply type "f" or type "rb!f"\n\n`
-                        +`\`${cfg.bot.prefix}ping\` - Ping the websocket!\n`
-                        +`\`${cfg.bot.prefix}info\` - Returns process information on the bot like uptime, guilds etc.\n`
-                        +`\`${cfg.bot.prefix}vote\` - Vote for the bot and support me!\n`
-                        +`\`${cfg.bot.prefix}help\` - Shows this page!\n`
-                        +`\`${cfg.bot.prefix}invite\` - Invite the bot to your server with this command, or [click here](\`${cfg.botinfo.invite_url}\`)\n\n`
-                        +`**Music Commands** Enabled: ${cfg.botinfo.music_status}\n`
-                        +`\`${cfg.bot.prefix}[play, p]\` - Play/Resume Music from Youtube.\n`
-                        +`\`${cfg.bot.prefix}[queue, q]\` - Show the current music queue and what is currently playing.\n`
-                        +`\`${cfg.bot.prefix}[skip, s]\` - Skip the current song.\n`
-                        +`\`${cfg.bot.prefix}[volume, vol]\` - Change the volume of the bot on the fly.\n`
-                        +`\`${cfg.bot.prefix}pause\` - Pause the current song.\n`
-                        +`\`${cfg.bot.prefix}stop\` - Stop the current song.\n`
-                        +`\`${cfg.bot.prefix}remove\` - Remove a song from the music queue.\n\n`
-                        +`**Need more help? Join the support server [here](https://discord.gg/CSJkCGx) or with this link\n<https://discord.gg/CSJkCGx>**`
-                        )
-                        .setFooter(`Owner: ${cfg.botinfo.owner}`)
-                )
+	async execute(client, msg) {
+        if(helpList.length === 0) await client.commands.forEach(function(value, key) { helpList.push(`\`${cfg.bot.prefix}${key}\` - ${value.description}`) })
+
+        let loading = await msg.channel.send(client.resource.loading())
+        loading.edit(client.resource.embed()
+            .setTitle('Respects Bot - Commands')
+            .setColor('#ffff4f')
+            .setTimestamp()
+            .setDescription(
+            `If you would like to pay your respects:tm: simply type "f" or type "rb!f"\n\n`
+            + helpList.join('\n') + `\n`
+            +`**Music Commands** Enabled: ${cfg.botinfo.music_status}\n`
+            +`\`${cfg.bot.prefix}[play, p]\` - Play/Resume Music from Youtube.\n`
+            +`\`${cfg.bot.prefix}[queue, q]\` - Show the current music queue and what is currently playing.\n`
+            +`\`${cfg.bot.prefix}[skip, s]\` - Skip the current song.\n`
+            +`\`${cfg.bot.prefix}[volume, vol]\` - Change the volume of the bot on the fly.\n`
+            +`\`${cfg.bot.prefix}pause\` - Pause the current song.\n`
+            +`\`${cfg.bot.prefix}stop\` - Stop the current song.\n`
+            +`\`${cfg.bot.prefix}remove\` - Remove a song from the music queue.\n\n`
+            +`**Need more help? Join the support server [here](https://discord.gg/CSJkCGx) or with this link\n<https://discord.gg/CSJkCGx>**`
+            )
+            .setFooter(`Owner: ${cfg.botinfo.owner}`)
+        )
 	},
 };
