8ce2c6f80800d503e3db43cd7c1ebaa745865e32
JayEffTree <jfinelli3@gmail.com>
2020-05-21 01:57:44 +0200

    leaderboards now retuirns up to top 10 results

--- commands/top.js
+++ commands/top.js
@@ -1,29 +1,47 @@
+let results = [];
+let limitArg = 5;
 module.exports = {
 	name: 'top',
 	description: 'Return a list of the top 5 guilds.',
-	async execute(client, msg) {
+	async execute(client, msg, commandArgs) {
         let loading = await msg.channel.send(client.resource.loading())
-        client.database.collection('guilds').find().limit(5).sort({fcount: -1}).toArray(function(err, result) {
-            console.log(result)
+        if(commandArgs.length === 0){
+            limitArg = 5;
+            return loading.edit(client.resource.embed()
+                .setTitle(`Error`)
+                .setDescription(`No Value Provided please use a value of 1-10`)
+            )
+        } else if(commandArgs > 10) {
+            return loading.edit(client.resource.embed()
+                .setTitle(`Error`)
+                .setDescription(`Please use a value of 1-10`)
+            )
+        } else {
+            limitArg = Number(commandArgs);
+        }
+        client.database.collection('guilds').find().limit(limitArg).sort({fcount: -1}).toArray(function(err, result) {
+            if(results.length === 0){
+                result.forEach(function(result, key){
+                    results.push(`${key + 1}. ${result.guild_name} - **${result.fcount}**`)
+                })
+            }else {
+                results = [];
+                result.forEach(function(result, key){
+                    results.push(`${key + 1}. ${result.guild_name} - **${result.fcount}**`)
+                })
+            }
             if (err){
                 loading.edit(client.resource.embed()
                     .setTitle(`Error`)
                     .setDescription(`Error found reporting this to my owner.:\n \`\`\`${err}\`\`\``)
                 )
-            } else{
+            } else {
                 loading.edit(client.resource.embed()
                     .setTitle(`Respects Bot Leaderboard`)
                     .setFooter(`Delivered via - MongoDB API`)
-                    .setDescription(`
-                    1. ${result[0].guild_name} - **${result[0].fcount.toLocaleString()}**
-                    2. ${result[1].guild_name} - **${result[1].fcount.toLocaleString()}**
-                    3. ${result[2].guild_name} - **${result[2].fcount.toLocaleString()}**
-                    4. ${result[3].guild_name} - **${result[3].fcount.toLocaleString()}**
-                    5. ${result[4].guild_name} - **${result[4].fcount.toLocaleString()}**`)
+                    .setDescription(results.join('\n'))
                 )
-                
             }
-            
         })
 	},
 };
