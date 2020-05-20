cc5a74b914101d2fb99b0d7344b8388f76de7493
JayEffTree <jfinelli3@gmail.com>
2020-05-21 00:25:24 +0200

    removing, due to recode inbound

--- resource/events/guildCreate.js
+++
@@ -1,9 +0,0 @@
-module.exports = (client, cfg) => {
-    
-    client.on("guildCreate", async (guild) => {
-        client.channels.cache.get(cfg.botinfo.guild_log_channel).send(client.resource.joinEmbed(guild));
-        setTimeout(async () => {
-            client.user.setPresence({ activity: { type: 'LISTENING', name: `${client.guilds.cache.size} servers. | rb!help` } })
-        }, 2000)
-    })
-};
