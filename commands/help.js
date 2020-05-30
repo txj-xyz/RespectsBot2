1a4be72eed0394728d685b9d292ec6026ee499af
JayEffTree <jfinelli3@gmail.com>
2020-05-30 09:48:21 +0200

    Cool pages for help embed

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,6 @@
 const cfg = require("../config/config.json")
 const helpList = [];
+const paginationEmbed = require('discord.js-pagination');
 
 module.exports = {
 	name: 'help',
@@ -7,24 +8,37 @@ module.exports = {
 	async execute(client, msg) {
         if(helpList.length === 0) await client.commands.forEach(function(value, key) { helpList.push(`\`${cfg.bot.prefix}${key}\` - ${value.description}`) })
 
-        let loading = await msg.channel.send(client.resource.loading())
-        loading.edit(client.resource.embed()
+        const help1 = client.resource.embed()
             .setTitle('Respects Bot - Commands')
             .setColor('#ffff4f')
             .setTimestamp()
             .setDescription(
-            `If you would like to pay your respects:tm: simply type "f" or type "rb!f"\n\n**General Commands:**\n`
-            + helpList.join('\n') + `\n\n`
-            +`**Music Commands**\n`
-            +`\`${cfg.bot.prefix}[play, p]\` - Play/Resume Music from Youtube.\n`
-            +`\`${cfg.bot.prefix}[queue, q]\` - Show the current music queue and what is currently playing.\n`
-            +`\`${cfg.bot.prefix}[skip, s]\` - Skip the current song.\n`
-            +`\`${cfg.bot.prefix}[volume, vol]\` - Change the volume of the bot on the fly.\n`
-            +`\`${cfg.bot.prefix}pause\` - Pause the current song.\n`
-            +`\`${cfg.bot.prefix}remove\` - Remove a song from the music queue.\n\n`
-            +`**Need more help? Join the support server [here](https://discord.gg/CSJkCGx) or with this link\n<https://discord.gg/CSJkCGx>**`
+                `If you would like to pay your respects:tm: simply type "f" or type "rb!f"\n\n`
+                + helpList.join('\n') + `\n\n`
+                +`**Need more help? Join the support server [here](https://discord.gg/CSJkCGx) or with this link\n<https://discord.gg/CSJkCGx>**`
             )
-            .setFooter(`Owner: ${cfg.botinfo.owner}`)
+        const help2 = client.resource.embed()
+            .setTitle('Respects Bot - Commands')
+            .setColor('#ffff4f')
+            .setTimestamp()
+            .setDescription(
+                `**Music Commands** Enabled: ${cfg.botinfo.music_status}\n`
+                +`\`${cfg.bot.prefix}[play, p]\` - Play/Resume Music from Youtube.\n`
+                +`\`${cfg.bot.prefix}[queue, q]\` - Show the current music queue and what is currently playing.\n`
+                +`\`${cfg.bot.prefix}[skip, s]\` - Skip the current song.\n`
+                +`\`${cfg.bot.prefix}[volume, vol]\` - Change the volume of the bot on the fly.\n`
+                +`\`${cfg.bot.prefix}pause\` - Pause the current song.\n`
+                +`\`${cfg.bot.prefix}stop\` - Stop the current song.\n`
+                +`\`${cfg.bot.prefix}remove\` - Remove a song from the music queue.\n\n`
+                +`**Need more help? Join the support server [here](https://discord.gg/CSJkCGx) or with this link\n<https://discord.gg/CSJkCGx>**`
+            )
+                
+        pages = [
+            help1,
+            help2
+        ];
+        client.resource.embed(
+            paginationEmbed(msg, pages)
         )
 	},
 };
