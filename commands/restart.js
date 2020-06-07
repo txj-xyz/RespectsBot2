f46af00e99bcf9b4b241632d563e88482fa08823
JayEffTree <jfinelli3@gmail.com>
2020-06-07 21:47:01 +0200

    idk why this wasn't in here, what am I doing jesus

--- commands/restart.js
+++ commands/restart.js
@@ -6,6 +6,7 @@ module.exports = {
 	name: 'restart',
 	description: 'Restart the bot with this command',
 	execute(_, msg) {
+		if(msg.author.id !== cfg.botinfo.ownerid) return msg.channel.send("HAHA NOT")
 		msg.channel.send({ embed : { description: `Shutting down, please wait 2 seconds and retry your commands...` } })
 		setTimeout(async () => {
 			process.exit(1)
